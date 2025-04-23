9957f52d5dde35880e7ca9fc3cdb2e4dcfeacb54
liurunyan <63230318+liurunyan@users.noreply.github.com>
2025-04-23 09:47:36 +0800

    Update LDVAE.py

--- comparison/LDVAE.py
+++ comparison/LDVAE.py
@@ -3,7 +3,7 @@
 
 # %%
 import os
-os.chdir("/root/data/DBP_sa_bc/")
+os.chdir(".../DBP/")
 from os.path import join as pj
 import argparse
 import sys
@@ -22,8 +22,8 @@ import scvi
 
 # %%
 parser = argparse.ArgumentParser()
-parser.add_argument('--task', type=str, default='wnn_rna')
-parser.add_argument('--experiment', type=str, default='e1')
+parser.add_argument('--task', type=str, default='pbmc')
+parser.add_argument('--experiment', type=str, default='alpha_13')
 parser.add_argument('--model', type=str, default='default')
 parser.add_argument('--init_model', type=str, default='sp_00001899')
 parser.add_argument('--method', type=str, default='LDVAE')
